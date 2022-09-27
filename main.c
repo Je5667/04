e6f723d3f0e31159c8544ab6c4bad7a0656bbf40
Je5667 <113067745+Je5667@users.noreply.github.com>
2022-09-27 17:02:07 +0900

    04 수정본

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int x;
 	
-	int x, y ;
    printf("input the year: ");
-   scanf("%d", &x);
-    y =((x%4==0&&x%100!=0)||x%400==0);
-   printf("is the year %d the leap year? : %d",x,y);
+   scanf("%i", &x);
+   
+   if((x%4==0 && x%100!=0)||x%400==0)
+    	printf("%i is leap year!\n",x);
+    else
+    	printf("%i is not leap year!\n",x);
    
    return 0;
-    
 }
